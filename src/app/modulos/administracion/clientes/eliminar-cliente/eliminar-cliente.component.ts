72faa56b100b74ca997a1d9987c2502d83ebfe6d
jhoncarlos79 <jhoncarlos@gmail.com>
2022-11-25 21:27:27 -0500

    eliminar-cliente

--- src/app/modulos/administracion/clientes/eliminar-cliente/eliminar-cliente.component.ts
+++ src/app/modulos/administracion/clientes/eliminar-cliente/eliminar-cliente.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ClienteService } from 'src/app/servicios/cliente.service';
 
 @Component({
   selector: 'app-eliminar-cliente',
   templateUrl: './eliminar-cliente.component.html',
   styleUrls: ['./eliminar-cliente.component.css']
 })
-export class EliminarClienteComponent {
+export class EliminarClienteComponent implements OnInit {
+  id:string = '';
+  
+  constructor(private clienteService:ClienteService, private router:Router, private route:ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params["id"];
+    this.EliminarCliente();
+  }
+
+  EliminarCliente(){
+    this.clienteService.EliminarCliente(this.id).subscribe((datos:any)=>{
+      this.router.navigate(["/administracion/buscar-cliente"]);
+    });
+  }
 
 }
